c282e081e3c94f0e13fb5d2f8962f700b730f58f
Iago <talamoniiago@gmail.com>
2023-02-14 21:23:24 +0000

    Criação dos modelos

---
+++ models/base/base_economy.sql
@@ -0,0 +1,12 @@
+with base_economy as (
+    
+    select _airbyte_economy_hashid,
+           gdp_usd,
+           location_key,
+           gdp_per_capita_usd,
+           human_capital_index
+    from {{source('covid19', 'economy')}} 
+)
+
+select *
+from base_economy
